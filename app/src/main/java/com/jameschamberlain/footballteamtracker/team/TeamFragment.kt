55acbeacd3b7d087d40aa3055e19a3bb2d3de292
James Chamberlain <james99chamberlain1999@gmail.com>
2022-04-23 13:47:00 +0100

    Fixed bug where the app would crash when submitting an empty player name.

--- app/src/main/java/com/jameschamberlain/footballteamtracker/team/TeamFragment.kt
+++ app/src/main/java/com/jameschamberlain/footballteamtracker/team/TeamFragment.kt
@@ -12,6 +12,7 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.NavHostFragment
@@ -32,7 +33,7 @@ import java.util.*
 private const val TAG = "TeamFragment"
 
 
-class TeamFragment : BaseFragment(), View.OnClickListener {
+class TeamFragment : BaseFragment() {
 
     private lateinit var adapter: PlayerAdapter
 
@@ -67,11 +68,7 @@ class TeamFragment : BaseFragment(), View.OnClickListener {
         adapter = PlayerAdapter(options, this@TeamFragment)
         binding.playersRecyclerView.adapter = adapter
 
-        if (Utils.getAccountType(requireActivity()) == AccountType.ADMIN) {
-            binding.fab.setOnClickListener(this)
-        } else {
-            binding.fab.visibility = View.GONE
-        }
+        setupFab()
     }
 
     fun addNoPlayersLayout() {
@@ -82,43 +79,75 @@ class TeamFragment : BaseFragment(), View.OnClickListener {
 
     fun removeNoPlayersLayout() { binding.noPlayersLayout.visibility = View.GONE }
 
-    override fun onClick(v: View) {
-        val editText = EditText(v.context)
-        editText.filters = arrayOf<InputFilter>(LengthFilter(maxNameLength))
-        editText.inputType = InputType.TYPE_TEXT_FLAG_CAP_WORDS
-        editText.hint = getString(R.string.name)
-        val scale = resources.displayMetrics.density
-        val dpAsPixels = (20 * scale + 0.5f).toInt()
-        val container = FrameLayout(requireContext())
-        val lp = FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        lp.setMargins(dpAsPixels, dpAsPixels, dpAsPixels, 0)
-        editText.layoutParams = lp
-        container.addView(editText)
-        MaterialAlertDialogBuilder(requireContext())
-                .setTitle(getString(R.string.add_player))
-                .setView(container)
-                .setPositiveButton(getString(R.string.confirm)) { _, _ ->
-                    val name = editText.text.toString()
-                    val playersRef = Utils.getTeamReference(requireActivity()).collection("players")
-                    playersRef.document(name.lowercase(Locale.ROOT)).get()
-                            .addOnCompleteListener { doc ->
-                                if (doc.isSuccessful) {
-                                    if (!doc.result!!.exists()) {
-                                        playersRef.document(name.lowercase(Locale.ROOT)).set(Player(name))
-                                                .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully added") }
-                                                .addOnFailureListener { e -> Log.w(TAG, "Error adding document", e) }
+
+    private fun setupFab() {
+        if (Utils.getAccountType(requireActivity()) == AccountType.ADMIN) {
+            binding.fab.setOnClickListener { v ->
+                val editText = EditText(v.context)
+                editText.filters = arrayOf<InputFilter>(LengthFilter(maxNameLength))
+                editText.inputType = InputType.TYPE_TEXT_FLAG_CAP_WORDS
+                editText.hint = getString(R.string.name)
+                val scale = resources.displayMetrics.density
+                val dpAsPixels = (20 * scale + 0.5f).toInt()
+                val container = FrameLayout(requireContext())
+                val lp = FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                lp.setMargins(dpAsPixels, dpAsPixels, dpAsPixels, 0)
+                editText.layoutParams = lp
+                container.addView(editText)
+                val dialog = MaterialAlertDialogBuilder(requireContext())
+                dialog
+                    .setTitle(getString(R.string.add_player))
+                    .setView(container)
+                    .setPositiveButton(getString(R.string.confirm)) { _, _ ->
+                        val name = editText.text.toString()
+                        if (name.isNotBlank()) {
+                            val playersRef =
+                                Utils.getTeamReference(requireActivity()).collection("players")
+                            playersRef.document(name.lowercase(Locale.ROOT)).get()
+                                .addOnCompleteListener { doc ->
+                                    if (doc.isSuccessful) {
+                                        if (!doc.result!!.exists()) {
+                                            playersRef.document(name.lowercase(Locale.ROOT))
+                                                .set(Player(name))
+                                                .addOnSuccessListener {
+                                                    Log.d(
+                                                        TAG,
+                                                        "DocumentSnapshot successfully added"
+                                                    )
+                                                }
+                                                .addOnFailureListener { e ->
+                                                    Log.w(
+                                                        TAG,
+                                                        "Error adding document",
+                                                        e
+                                                    )
+                                                }
+                                        } else {
+                                            Toast.makeText(
+                                                requireContext(),
+                                                "Can't add player: \"$name\" already exists.",
+                                                Toast.LENGTH_LONG
+                                            ).show()
+                                        }
                                     } else {
-                                        Toast.makeText(requireContext(), "Can't add player: \"$name\" already exists.", Toast.LENGTH_LONG).show()
+                                        Toast.makeText(
+                                            requireContext(),
+                                            "Can't add player: network error.",
+                                            Toast.LENGTH_LONG
+                                        ).show()
                                     }
                                 }
-                                else {
-                                    Toast.makeText(requireContext(), "Can't add player: network error.", Toast.LENGTH_LONG).show()
-                                }
-                            }
-                }
-                .setNegativeButton(getString(R.string.cancel), null)
-                .show()
+                        }
+                    }
+                    .setNegativeButton(getString(R.string.cancel), null)
+                    .show()
+            }
+        } else {
+            binding.fab.visibility = View.GONE
+        }
     }
+
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
